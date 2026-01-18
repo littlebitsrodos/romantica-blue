e68b70e2f5f4ca9f6bcb31532cda55d839096b09
littlebitsrodos <littlebits.rodos@gmail.com>
2026-01-18 14:10:54 +0200

    fix: Update CSP and move SW registration to script.js

--- script.js
+++ script.js
@@ -919,3 +919,12 @@ async function loadAllBookings() {
   renderCalendar();
 }
 */
+
+// ----- Service Worker Registration (PWA) -----
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(registration => console.log('ServiceWorker registration successful'))
+            .catch(err => console.log('ServiceWorker registration failed: ', err));
+    });
+}
